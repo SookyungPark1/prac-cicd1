dc76e67bc7699918286e5af4d055f6bffa768d8c
Your Name <you@example.com>
2025-06-10 10:27:00 +0900

    test1

--- src/main/java/com/example/prac_cicd1/HelloController.java
+++ src/main/java/com/example/prac_cicd1/HelloController.java
@@ -2,7 +2,16 @@ package com.example.prac_cicd1;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class HelloController {
+
+    @GetMapping("/hi")
+    public String hi(){
+        return "hi cicd prac1";
+    }
+
+
 }
