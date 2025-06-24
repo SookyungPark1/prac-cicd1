ba3ae44b3d45e2203e0e5edac3b8a44b5e77ee85
sookyung <sookyung963@naver.com>
2025-06-24 11:40:24 +0900

    dfd'

--- src/main/java/com/example/prac_cicd4/HelloController.java
+++ src/main/java/com/example/prac_cicd4/HelloController.java
@@ -9,6 +9,6 @@ public class HelloController
 
     @GetMapping("/hi")
     public  String  hi(){
-        return  "hi cicd ^^   코드 수정 ec2 성능 업";
+        return  "hi cicd ^^   코드 수정 ec2 성능 업   chmod ^^";
     }
 }
